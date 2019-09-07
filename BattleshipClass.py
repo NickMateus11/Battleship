c56b554f3975ca21417d994de34aba9985eb2c5d
Nicholas Mateus <nick.mateus@MBA-20141011.local>
2019-09-07 14:27:25 -0400

    double board display

--- BattleshipClass.py
+++ BattleshipClass.py
@@ -13,14 +13,16 @@ class Battleship:
 
     def draw_board(self):
         print('\n'*3,end='')
-        print(f'{" "*3}| {" ".join(Battleship.COL_LABELS)} |')
+        print(f'{" "*3}| {" ".join(Battleship.COL_LABELS)} |',end='\t') # Self ships
+        print(f'{" "*3}| {" ".join(Battleship.COL_LABELS)} |')          # Attacks
         cap_row = f'{"-"*3}|{"-"*(Battleship.BOARD_DIM*2+1)}|{"-"*3}'
         basic_row = f'|{" "*(Battleship.BOARD_DIM*2+1)}|'
-        print(cap_row)
+        print(cap_row + '\t' + cap_row)
         for x in range(1,11):
             spacing = ' ' if x<10 else ''
-            print(f'{spacing}{x} {self._inject_ships(basic_row, x)}')
-        print(cap_row,end='\n\n')
+            print(f'{spacing}{x} {self._inject_ships(basic_row, x)}',end='\t')
+            print(f'{spacing}{x} {self._inject_attacks(basic_row, x)}')
+        print(cap_row + '\t' + cap_row,end='\n\n')
         # print(f'{" "*3}|{" "*(Battleship.BOARD_DIM*2+1)}|')
     
     def _inject_ships(self, basic_row_text, row):
@@ -28,7 +30,15 @@ class Battleship:
             if ship_seg_index//Battleship.BOARD_DIM == row-1:
                 row_index = ship_seg_index%Battleship.BOARD_DIM*2+1
                 basic_row_text = basic_row_text[0:row_index+1] + '@' + \
-                     basic_row_text[row_index+2:]
+                     basic_row_text[row_index+2:] # TODO: enable different symbols for each ship
+        return basic_row_text
+    
+    def _inject_attacks(self, basic_row_text, row):
+        for ship_seg_index in self.ships:
+            if ship_seg_index//Battleship.BOARD_DIM == row-1:
+                row_index = ship_seg_index%Battleship.BOARD_DIM*2+1
+                basic_row_text = basic_row_text[0:row_index+1] + chr(2155) + \
+                    basic_row_text[row_index+2:] # TODO: enable different symbols for each ship
         return basic_row_text
 
 
