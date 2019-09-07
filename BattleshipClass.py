9bf1db30f8fbd0b0ef06f651af38dfe15addda7d
Nicholas Mateus <nick.mateus@MBA-20141011.local>
2019-09-07 14:03:24 -0400

    drawing ships

--- BattleshipClass.py
+++ BattleshipClass.py
@@ -2,16 +2,16 @@ class Battleship:
     BOARD_DIM = 10
     COL_LABELS = [chr(ord('A')+i) for i in range(10)]
     def __init__(self,init_ships=[]):
-        self.board = [self._decode(coord) for coord in init_ships]
+        self.ships = [self._decode(coord) for coord in init_ships]
         self.attack_board = []
     
     def _decode(self, coord):
-        col = 'a' - coord[0]
-        row = coord[1]
-        return col + row * Battleship.BOARD_DIM
+        col = ord(coord[0].lower()) - ord('a')
+        row = int(coord[1:])-1
+        print(col,row)
+        return col + (row * Battleship.BOARD_DIM)
 
-    @staticmethod
-    def draw_board(board=[]):
+    def draw_board(self):
         print('\n'*3,end='')
         print(f'{" "*3}| {" ".join(Battleship.COL_LABELS)} |')
         cap_row = f'{"-"*3}|{"-"*(Battleship.BOARD_DIM*2+1)}|{"-"*3}'
@@ -19,8 +19,17 @@ class Battleship:
         print(cap_row)
         for x in range(1,11):
             spacing = ' ' if x<10 else ''
-            print(f'{spacing}{x} {basic_row}')
+            print(f'{spacing}{x} {self._inject_ships(basic_row, x)}')
         print(cap_row,end='\n\n')
         # print(f'{" "*3}|{" "*(Battleship.BOARD_DIM*2+1)}|')
+    
+    def _inject_ships(self, basic_row_text, row):
+        for ship_seg_index in self.ships:
+            if ship_seg_index//Battleship.BOARD_DIM == row-1:
+                row_index = ship_seg_index%Battleship.BOARD_DIM*2+1
+                basic_row_text = basic_row_text[0:row_index+1] + '@' + \
+                     basic_row_text[row_index+2:]
+        return basic_row_text
 
-Battleship().draw_board()
+
+Battleship(['a1','b2','d5','d6','d7','j1','j10','A10']).draw_board()
